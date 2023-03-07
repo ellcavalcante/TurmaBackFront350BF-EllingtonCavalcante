ed56a455f919f0f3c5e0be9f704c9879d2e220c3
Ellington Cavalcante <ellcavalcante778@gmail.com>
2023-03-07 20:46:46 -0300

    Criando classes e atributos

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -7,5 +7,61 @@
 
 import Foundation
 
-print("Hello, World!")
+// OBJETO
+// CARACTERISTICAS DE UM OBJETO -> ATRIBUTOS OU PROPRIEDADES
+// AÇÕES = FUNÇÃO / METODO
+
+// Para criar uma classe sempre começamos com a palavra reservada "class" e em seguida colocamos o seu nome que sempre deve começar com a primeira letra maiusculo
+
+class Automovel {
+    
+    var cor: String = "Preto"
+    var numeroDePortas: Int = 2
+    var marca: String = "Honda"
+    var eEletrico: Bool = false
+    var quantidadeDePassageiros: Int = 5
+    
+    func ligarFarol() {
+        print("O Farol está ligado")
+    }
+    
+    func ligarOCarro() {
+        print("O carro está ligado")
+    }
+    
+}
+
+class House {
+    
+    var qDoors: Int = 10
+    var color: String = "White"
+    var haveSolarEnergy: Bool = true
+    var haveGarage: Bool = true
+    var havePool: Bool = true
+    
+    func openGarage() {
+        print("Abrir a garagem")
+    }
+    
+    func cleanPool() {
+        print("Limpar piscina")
+    }
+}
+
+class Mac {
+    
+    var isRetina: Bool = true
+    var color: String = "Space gray"
+    var inch: Int = 16
+    var chip: String = "M2"
+    var haveTouchBar: Bool = true
+    
+    func turnOnMac() {
+        print("Ligar o Macbook")
+    }
+    
+    func acessTheInternet() {
+        print("Abrir o navegador padrão")
+    }
+}
 
