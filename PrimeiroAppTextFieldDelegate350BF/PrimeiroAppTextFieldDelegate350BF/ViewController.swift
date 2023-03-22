b9cbc99430dc1dc01cff564add2484c4bd2fb94f
Ellington Cavalcante <ellcavalcante778@gmail.com>
2023-03-21 23:30:55 -0300

    Atualizando o codigo e subindo pro git

--- PrimeiroAppTextFieldDelegate350BF/PrimeiroAppTextFieldDelegate350BF/ViewController.swift
+++ PrimeiroAppTextFieldDelegate350BF/PrimeiroAppTextFieldDelegate350BF/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.placeholder = "Digite seu nome:"
+        emailTextField.placeholder = "Digite seu e-mail:"
+        nameTextField.delegate = self
+        emailTextField.delegate = self
     }
-
-
+    
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    // Quando o teclado subir esse metodo será disparado
+    // didBegin -> autocomplete
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing")
+        if textField == nameTextField {
+            nameTextField.layer.borderColor = UIColor.orange.cgColor
+            nameTextField.layer.borderWidth = 2
+        } else {
+            emailTextField.layer.borderColor = UIColor.purple.cgColor
+            emailTextField.layer.borderWidth = 2
+        }
+    }
+    
+    // Quando o teclado abaixa/some da tela
+    // Metodo utilizado para fazer validações de campo
+    // didEndEdit
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+        textField.layer.borderWidth = 0
+        if nameTextField.hasText == true && emailTextField.hasText == true {
+            view.backgroundColor = .blue
+        } else {
+            view.backgroundColor = .red
+        }
+    }
+    
+    // Quando pressionamos no botão "RETURNO"
+    // return
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true
+    }
+}
