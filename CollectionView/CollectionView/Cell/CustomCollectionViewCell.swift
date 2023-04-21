398bb6e3cae648e20a8b9bcce1c651c5139e41db
Ellington Cavalcate <89115923+ellcavalcante@users.noreply.github.com>
2023-04-20 21:01:20 -0300

    Criando app com CollectionView

---
+++ CollectionView/CollectionView/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Ellington Cavalcante on 20/04/23.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
